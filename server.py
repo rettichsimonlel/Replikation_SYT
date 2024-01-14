4f597044895a2547b2c22e38cf8d5db82ac1989e
rettichsimonlel <retter190085@sr.htlweiz.at>
2024-01-14 19:32:32 +0100

    keeping the socket alive

--- server.py
+++ server.py
@@ -6,12 +6,18 @@ import sqlite3
 HOST = '127.0.0.1'
 PORT = 5555
 
+# Socket setup
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
+
 # Database setup
 def create_database_connection(database_name):
     conn = sqlite3.connect(database_name)
     cursor = conn.cursor()
     return conn
 
+# Sync the database
 def sync_database(db_write, db_read):
     # Lock to ensure exclusive access to the databases
     with threading.Lock():
@@ -41,54 +47,53 @@ def sync_database(db_write, db_read):
             conn_write.close()
             conn_read.close()
 
-# Socket setup
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
-
 # Client handling function
 def handle_client(client_socket):
     try:
-        # Read or Write database selection
-        selection = client_socket.recv(1024).decode('utf-8')
+        while True:
+            # Read or Write database selection
+            selection = client_socket.recv(1024).decode('utf-8')
+            print(f"selection r/w: {selection}")
 
-        if selection == 'r':
-            print("In reading")
-            sync_database('write_database.db', 'read_database.db')
+            if selection == 'r':
+                print("In reading")
+                sync_database('write_database.db', 'read_database.db')
 
-            db_conn_read = create_database_connection('read_database.db')
-            db_cursor_read = db_conn_read.cursor()
+                db_conn_read = create_database_connection('read_database.db')
+                db_cursor_read = db_conn_read.cursor()
 
-            # Read data from the write database and send it to the client
-            db_cursor_read.execute('SELECT * FROM data')
-            result_read = db_cursor_read.fetchall()
-            client_socket.send(str(result_read).encode('utf-8'))
+                # Read data from the write database and send it to the client
+                db_cursor_read.execute('SELECT * FROM data')
+                result_read = db_cursor_read.fetchall()
+                client_socket.send(str(result_read).encode('utf-8'))
 
-            db_conn_read.close()
+                db_conn_read.close()
 
-        elif selection == 'w':
-            print("In writing")
-            db_conn_write = create_database_connection('write_database.db')
-            db_cursor_write = db_conn_write.cursor()
+            elif selection == 'w':
+                print("In writing")
+                db_conn_write = create_database_connection('write_database.db')
+                db_cursor_write = db_conn_write.cursor()
 
-            data = client_socket.recv(1024).decode('utf-8')
+                data = client_socket.recv(1024).decode('utf-8')
+                print(f"data into database: {data}")
 
-            # Write data to the write database
-            db_cursor_write.execute('INSERT INTO data (value) VALUES (?)', (data,))
-            db_conn_write.commit()
-            print(f"Value: {data} written in the database")
+                # Write data to the write database
+                db_cursor_write.execute('INSERT INTO data (value) VALUES (?)', (data,))
+                db_conn_write.commit()
+                print(f"Value: {data} written in the database")
 
-            sync_database('write_database.db', 'read_database.db')
+                sync_database('write_database.db', 'read_database.db')
 
-            # Read data from the write database and send it to the client
-            db_cursor_write.execute('SELECT * FROM data')
-            result_write = db_cursor_write.fetchall()
-            client_socket.send(str(result_write).encode('utf-8'))
+                # Read data from the write database and send it to the client
+                db_cursor_write.execute('SELECT * FROM data')
+                result_write = db_cursor_write.fetchall()
+                client_socket.send(str(result_write).encode('utf-8'))
 
-            db_conn_write.close()
+                db_conn_write.close()
 
-        else:
-            raise ValueError("Invalid selection")
+            else:
+                raise ValueError("Invalid selection")
+                break
 
     except KeyboardInterrupt:
         print("Server interrupted. Closing socket...")
